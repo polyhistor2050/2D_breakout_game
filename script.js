2747aaa6a2796d0f81a7713e3d0996958dd15a80
polyhistor2050 <chrisspaul2050@gmail.com>
2023-07-22 15:07:26 +0300

    bounce the ball of the canvas walls

--- script.js
+++ script.js
@@ -5,22 +5,35 @@ let x = canvas.width / 2;
 let y = canvas.height - 30;
 let dx = 2;
 let dy = -2;
+let ballRadius = 10;
 
+//draw ball
 function drawBall(){
     ctx.beginPath();
-    ctx.arc(x, y, 10, 0, Math.PI * 2);
+    ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
     ctx.fillStyle = "#0095DD";
     ctx.fill();
     ctx.closePath();
 }
 
+//clear ball
 function draw(){
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawBall();
-
     //update x and y position
     x += dx;
     y += dy;
+    bounce();
+}
+
+//bounce ball
+function bounce(){
+    if(y + dy < ballRadius || y + dy > canvas.height - ballRadius){   //bouncing off the top and bottom
+        dy = -dy;
+    }
+    if(x + dx < ballRadius || x + dy > canvas.width - ballRadius){   //bouncing off the left and right
+        dx = -dx;
+    }
 }
 
 setInterval(draw, 10); //call the function every 10 milliiseconds
