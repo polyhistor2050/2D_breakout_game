834caf19671184b37b37cbc4c5b0cf8cc2895568
polyhistor2050 <chrisspaul2050@gmail.com>
2023-07-22 14:19:56 +0300

    draw and move the ball on canvas

--- script.js
+++ script.js
@@ -1,20 +1,26 @@
 let canvas = document.getElementById("myCanvas");
 let ctx = canvas.getContext("2d");
 
-ctx.beginPath();
-ctx.rect(20, 40, 50, 50);
-ctx.fillStyle = "#FF0000";
-ctx.fill();
-ctx.closePath();
+let x = canvas.width / 2;
+let y = canvas.height - 30;
+let dx = 2;
+let dy = -2;
 
-ctx.beginPath();
-ctx.arc(240, 160, 50, 0, Math.PI * 2, false);
-ctx.fillStyle = "green";
-ctx.fill();
-ctx.closePath();
+function drawBall(){
+    ctx.beginPath();
+    ctx.arc(x, y, 10, 0, Math.PI * 2);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+}
 
-ctx.beginPath();
-ctx.rect(160, 10, 100, 40);
-ctx.strokeStyle = "rgba(0, 0, 255, 0.5)";
-ctx.stroke();
-ctx.closePath();
+function draw(){
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBall();
+
+    //update x and y position
+    x += dx;
+    y += dy;
+}
+
+setInterval(draw, 10); //call the function every 10 milliiseconds
